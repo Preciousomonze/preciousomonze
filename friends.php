f1933f04cd22dbf3e79feec413f6b2d313377bc1
Precious Omonzejele <crispusomonze@gmail.com>
2021-10-25 10:40:35 +0100

    Update friends.php

--- friends.php
+++ friends.php
@@ -1,6 +1,6 @@
 <?php
-  require ('friend_vibes.php');
-  require ('outing_activities.php');
+  require ('~friend_vibes.php');
+  require ('~outing_activities.php');
 
   if (precious_going() && (joke_going() && sodiq_going() && others_going())) {
     do_outing();
